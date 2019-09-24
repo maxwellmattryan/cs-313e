6ac301e92efbd644630d0b905a9d8798c6b0ea9f
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-09-23 20:18:49 -0500

    Finish assignment 6, start 7

--- assignments/6-office-space/OfficeSpace.py
+++ assignments/6-office-space/OfficeSpace.py
@@ -14,125 +14,92 @@
 
 #  Date Last Modified: 09-23-2019
 
-import math
-
-class Point (object):
-  # constructor 
-  # x and y are floats
-  def __init__ (self, x = 0, y = 0):
-    self.x = x
-    self.y = y
-
-  # get distance
-  # other is a Point object
-  def dist (self, other):
-    return math.hypot (self.x - other.x, self.y - other.y)
-
-  # get a string representation of a Point object
-  # takes no arguments
-  # returns a string
-  def __str__ (self):
-    return '(' + str(self.x) + ", " + str(self.y) + ")"
-
-  # test for equality
-  # other is a Point object
-  # returns a Boolean
-  def __eq__ (self, other):
-    tol = 1.0e-8
-    return ((abs (self.x - other.x) < tol) and (abs(self.y - other.y) < tol))
-
-class Rectangle (object):
-  # constructor
-  def __init__ (self, ll_x = 0, ll_y = 0, ur_x = 1, ur_y = 1):
-    if ((ll_x < ur_x) and (ll_y < ur_y)):
-      self.ll = Point (ll_x, ll_y)
-      self.ur = Point (ur_x, ur_y)
-    else:
-      self.ll = Point (0, 0)
-      self.ur = Point (1, 1)
-
-  # determine length of Rectangle (distance along the x axis)
-  # takes no arguments, returns a float
-  def length (self):
-    return(self.ur.x - self.ll.x)
-
-  # determine width of Rectangle (distance along the y axis)
-  # takes no arguments, returns a float
-  def width (self):
-    return(self.ur.y - self.ll.y)
-
-  # determine the perimeter
-  # takes no arguments, returns a float
-  def perimeter (self):
-    return(2 * (self.length() + self.width()))
-    
-  # determine the area
-  # takes no arguments, returns a float
-  def area (self):
-    return(self.length() * self.width())
-
-  # determine if a point is strictly inside the Rectangle
-  # takes a point object p as an argument, returns a boolean
-  def point_inside (self, p):
-    return(self.ur.x > p.x > self.ll.x and self.ur.y > p.y > self.ll.y)
-
-  # determine if another Rectangle is strictly inside this Rectangle
-  # takes a rectangle object r as an argument, returns a boolean
-  # should return False if self and r are equal
-  def rectangle_inside (self, r):
-    return(self.ll.x < r.ll.x < r.ur.x < self.ur.x and self.ll.y < r.ll.y < r.ur.y < self.ur.y)
-
-  # determine if two Rectangles overlap (non-zero area of overlap)
-  # takes a rectangle object r as an argument returns a boolean
-  def rectangle_overlap (self, r):
-      print("FIXME: Complete rectangle overlap function...")
-      return(-1)
-
-  # give string representation of a rectangle
-  # takes no arguments, returns a string
-  def __str__ (self):
-    return "LL: " + str(self.ll) + ", UR: " + str(self.ur)
-
-  # determine if two rectangles have the same length and width
-  # takes a rectangle other as argument and returns a boolean
-  def __eq__ (self, other):
-    return(self.ll == other.ll and self.ur == other.ur)
-
 # read data from file
 def readFile():
     myFile = open("office.txt", "r")
+    dataSet = []
     data = []
     lines = [line.strip() for line in myFile]
-    for i in range(len(lines)):
+    i = 0
+    while(i < len(lines)):
         if(i == 0):
             points = lines[i].split(" ")
             data.append((int(points[0]), int(points[1])))
-        if(i == 1):
+            i += 1
+        elif(i == 1):
             for j in range(2, 2 + int(lines[i])):
                 employee = lines[j].split(" ")
                 name = employee[0]
-                requestedSpace = Rectangle(int(employee[1]), int(employee[2]), int(employee[3]), int(employee[4]))
-                data.append([name, requestedSpace])
+                lowerLeftPoint = [int(employee[1]), int(employee[2])]
+                upperRightPoint = [int(employee[3]), int(employee[4])]
+                data.append([name, lowerLeftPoint, upperRightPoint])
+                i += 1
+            dataSet.append(data)
+            data = []
+            lines = lines[i + 1:]
+            i = 0
+    print()
     myFile.close()
-    [print(item) for item in data]
-    return(data)
+    return(dataSet)
+
+# create and populate office grid with requests
+def makeOfficeGrid(myData):
+    # fill grid with zeros
+    grid = [[0 for j in range(myData[0][0])] for i in range(myData[0][1])]
+
+    # populate grid from employee requests
+    colOffset = len(grid) # used for subtracting the y coordinates from grid length to get correct grid coordinates
+    for employee in myData[1:]:
+        for i in range(colOffset - employee[2][1], colOffset - employee[1][1]):
+            for j in range(employee[1][0], employee[2][0]):
+                grid[i][j] += 1
+    return(grid)
+
+# return integer of amount of contested space
+def processRequests(grid, employees):
+    unallocatedSpace = 0
+    contestedSpace = 0
+    for row in grid:
+        for column in row:
+            if(column == 0):
+                unallocatedSpace += 1
+            elif(column >= 2):
+                contestedSpace += 1
+    colOffset = len(grid)
+    for employee in employees:
+        employeeSpace = 0
+        for i in range(colOffset - employee[2][1], colOffset - employee[1][1]):
+            for j in range(employee[1][0], employee[2][0]):
+                if(grid[i][j] == 1):
+                    employeeSpace += 1
+        employee.append(employeeSpace)
+
+    return(unallocatedSpace, contestedSpace, employees)
 
 # print data according to format
-def printData(myData):
+def printData(myData, unallocatedSpace, contestedSpace):
     totalArea = myData[0][0] * myData[0][1]
     # total area of office space (x times y)
     print("Total " + str(totalArea))
     # unallocated area of office space (total minus area of squares)
-    totalRequestedArea = sum([employee[1].area() for employee in myData[1:]])
-    print("FIXME: Unallocated office space is " + str(totalArea - totalRequestedArea))
+    print("Unallocated " + str(unallocatedSpace))
     # contested area of office space (find total area of rectangular overlap)
-    print("FIXME: Contested office space")
+    print("Contested " + str(contestedSpace))
     # each employee with guaranteed space (original request minus contested) * same order as input
-    for employee in myData[1:]:
-        print("FIXME: " + employee[0] + "'s guaranteed space is " + str(employee[1].area()))
+    [print(employee[0] + " " + str(employee[3])) for employee in myData[1:]]
+    print()
+
+# tool to print and check the grid
+def printGrid(grid):
+    print("Dimensions: " + str(len(grid[0])) + " x " + str(len(grid)))
+    [print(unit) for unit in grid]
+    print()
 
 def main():
-    myData = readFile()
-    printData(myData)
+    myDataSet = readFile()
+    for myData in myDataSet:
+        officeGrid = makeOfficeGrid(myData)
+        unallocatedSpace, contestedSpace, myData[1:] = processRequests(officeGrid, myData[1:])
+        printData(myData, unallocatedSpace, contestedSpace)
 
 main()
---
+++ assignments/7-convex-hull/ConvexHull.py
@@ -0,0 +1,20 @@
+#  File: ConvexHull.py
+
+#  Description: Assignment 07 | Convex Hull
+
+#  Student Name: Matthew Maxwell
+
+#  Student UT EID: mrm5632
+
+#  Course Name: CS 313E
+
+#  Unique Number: 50205
+
+#  Date Created: 09-23-2019
+
+#  Date Last Modified: 09-23-2019
+
+def main():
+    print("FIXME: do assignment lolol")
+
+main()
