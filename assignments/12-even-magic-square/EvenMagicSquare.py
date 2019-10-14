2b2bf18761968465f44615306c2b5a9c855a2e6b
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-10-13 22:31:48 -0500

    Start assignment 12

---
+++ assignments/12-even-magic-square/EvenMagicSquare.py
@@ -0,0 +1,26 @@
+#  File: EvenMagicSquare.py
+
+#  Description: Assignment 12 | Even Magic Square
+
+#  Student Name: Matthew Maxwell
+
+#  Student UT EID: mrm5632
+
+#  Course Name: CS 313E
+
+#  Unique Number: 50205 
+
+#  Date Created: 10-13-2019
+
+#  Date Last Modified: 10-13-2019
+
+def main():
+    # Create 1D list of integers 1-16
+    myList = [i + 1 for i in range(16)]
+    print(myList)
+    # Permute this list
+    # For each permutation convert the 1D list into a 2D list (4x4)
+    # Print list (1D) if its 2D version is a magic square
+    # Stop when you have gone through all permutations
+    print(f"FIXME: Start assignment")
+main()
