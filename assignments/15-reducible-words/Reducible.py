f07ba96dd6d3319cb063e0d80e6b1bc1c26b6de3
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-10-27 23:54:19 -0500

    Complete most functions for assignment 15, finish tomorrow

--- assignments/15-reducible-words/Reducible.py
+++ assignments/15-reducible-words/Reducible.py
@@ -10,9 +10,9 @@
 
 #  Unique Number: 50205 
 
-#  Date Created: 10-24-2019
+#  Date Created: 10-27-2019
 
-#  Date Last Modified: 10-24-2019
+#  Date Last Modified: 10-27-2019
 
 # takes as input a string and a hash table and returns True
 # if the string is in the hash table and False otherwise
@@ -26,29 +26,51 @@ def get_longest_words (string_list):
 
 # goes through input file and returns the word list
 def get_word_list():
-    print("FIXME: Define get_word_list()")
-    return(-1)
-    # create an empty word_list
-    # open the file words.txt
-    # read words from words.txt and append to word_list
-    # close file words.txt
+    myFile = open("words.txt", "r")
+    words = [line.strip() for line in myFile]
+    myFile.close()
+    return(words)
 
 # takes as input a string in lower case and the size
 # of the hash table and returns the index the string
 # will hash into
 def hash_word (s, size):
-    print("FIXME: Define: hash_word()")
+    hashIndex = 0
+    for j in range(len(s)):
+        letter = ord(s[j]) - 96
+        hashIndex = (hashIndex * 26 + letter) % size
+    return(hashIndex)
 
 # takes as input a string and a hash table and enters
 # the string in the hash table, it resolves collisions
 # by double hashing
 def insert_word (s, hash_table):
-    print("FIXME: Define: insert_word()")
+    # create initial index and test until there is a valid index
+    index1 = hash_word(s, len(hash_table))
+    if(hash_table[index1] != ""):
+        index2 = 13 - (index1 % 13)
+        i = 0
+        while(True):
+            newindex = (index1 + i * index2) % len(hash_table)
+            if(hash_table[newindex] == ""):
+                hash_table[newindex] = s
+                break
+            i += 1
+    else:
+        hash_table[index1]
 
 # takes as input a positive integer n
 # returns True if n is prime and False otherwise
 def is_prime (n):
-    print("FIXME: Define: is_prime()")
+    if(n == 1):
+        return(False)
+    limit = int(n ** 0.5) + 1
+    div = 2
+    while(div < limit):
+        if(n % div == 0):
+            return(False)
+        div += 1
+    return(True)
 
 # recursively finds if a word is reducible, if the word is
 # reducible it enters it into the hash memo and returns True
@@ -56,20 +78,28 @@ def is_prime (n):
 def is_reducible (s, hash_table, hash_memo):
     print("FIXME: Define is_reducible()")
 
+# takes as input a string in lower case and the constant
+# for double hashing and returns the step size for that 
+# string
+def step_size (s, const):
+    return(-1)
+
 def main():
-    word_list = get_word_list()
-    print(word_list)
-    # find length of word_list
+    # read data in as the list of curated words
+    words = get_word_list()
+    [print(word) for word in words]
 
-    # determine prime number N that is greater than twice
-    # the length of the word_list
+    # determine a prime number that is greater than twice the length of the words
+    primeNum = len(words) * 2 + 1
+    while(not is_prime(primeNum)):
+        primeNum += 1
 
-    # create and empty hash_list
+    # create an empty hash list with the prime number of blank strings
+    h_words = ["" for i in range(primeNum)]
 
-    # populate the hash_list with N blank strings
-
-    # hash each word in word_list into hash_list
-    # for collisions use double hashing 
+    # hash each word in words into h_words for collisions using double hashing 
+    for word in words:
+        insert_word(word, h_words)
 
     # create an empty hash_memo
 
