ae9dfffeb7fc0512fc6e6ed8163f7d6c30305bfa
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-10-31 00:20:56 -0500

    Begin a17 (Josephus

---
+++ assignments/17-josephus/Josephus.py
@@ -0,0 +1,49 @@
+#  File: Josephus.py
+
+#  Description: Assignment 17 | Josephus Problem (Circularly Linked List)
+
+#  Student Name: Matthew Maxwell
+
+#  Student UT EID: mrm5632
+
+#  Course Name: CS 313E
+
+#  Unique Number: 50205 
+
+#  Date Created: 10-30-2019
+
+#  Date Last Modified: 10-30-2019
+
+class Link(object):
+    ...
+
+class CircularList(object):
+    # Constructor
+    def __init__ ( self ): 
+        ...
+
+    # Insert an element (value) in the list
+    def insert ( self, data):
+        ...
+
+    # Find the link with the given data (value)
+    def find ( self, data ):
+        ...
+
+    # Delete a link with a given data (value)
+    def delete ( self, data ):
+        ...
+
+    # Delete the nth link starting from the Link start 
+    # Return the next link from the deleted Link
+    def delete_after ( self, start, n ):
+        ...
+
+    # Return a string representation of a Circular List
+    def __str__ ( self ):
+        ...
+
+def main():
+    ...
+
+main()
