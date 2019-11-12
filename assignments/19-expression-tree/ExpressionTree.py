4fb4b89567eb9ed311dbc83d1ab36f0e3b984f96
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-11-11 23:58:04 -0600

    Nearly complete a19, update todo (include support for floating point nums)

--- assignments/19-expression-tree/ExpressionTree.py
+++ assignments/19-expression-tree/ExpressionTree.py
@@ -53,10 +53,14 @@ class Node (object):
             levels=[self]
         nextLevel=[]
         for node in levels:
-            print(node.data, end=" ")
+            if(node == None):
+                print(" ", end=" ")
+            else:
+                print(node.data, end=" ")
         for node in levels:
-            nextLevel.extend([node.left, node.right])
-        print('\n')
+            if(node != None):
+                nextLevel.extend([node.left, node.right])
+        print()
         if nextLevel:
             node.print(nextLevel) 
 
@@ -67,31 +71,54 @@ class Tree (object):
         self.root = Node(None)
         self.operators = ["+", "-", "*", "/", "//", "%", "**"]
 
-    def create_tree (self, expr):
+    def create_tree (self, expr_list):
         current = self.root
         expr_stack = Stack()
-        for exp_char in expr:
-            if exp_char == "(":
+        for exp_token in expr_list:
+            if exp_token == "(":
                 current.left = Node(None)
                 expr_stack.push(current)
                 current = current.left
-            elif exp_char in self.operators:
-                current.data = exp_char
+            elif exp_token in self.operators:
+                current.data = exp_token
                 current.right = Node(None)
                 expr_stack.push(current)
                 current = current.right
-            elif exp_char == ")":
+            elif exp_token == ")":
                 if expr_stack.size() > 0:
                     current = expr_stack.pop()
             else:
-                current.data = int(exp_char)
+                current.data = int(exp_token)
                 current = expr_stack.pop()
 
-    def evaluate (self, aNode):
-        ...
-        
+    def evaluate(self, aNode):
+        if aNode.data in self.operators:
+            result = self.operate(self.evaluate(aNode.left), self.evaluate(aNode.right), aNode.data)
+            return result
+        return aNode.data
+
+    # return result of mathematical operation
+    def operate(self, operand1, operand2, operator):
+        if operator == "+":
+            return operand1 + operand2
+        elif operator == "-":
+            return operand1 - operand2
+        elif operator == "*":
+            return operand1 * operand2
+        elif operator == "/":
+            return operand1 / operand2
+        elif operator == "//":
+            return operand1 // operand2
+        elif operator == "%":
+            return operand1 % operand2
+        elif operator == "**":
+            return operand1 ** operand2
+
     def pre_order (self, aNode):
-        ...
+        if(aNode != None):
+            print(aNode.data, end= " ")
+            self.pre_order(aNode.left)
+            self.pre_order(aNode.right)
 
     # print the tree
     def print(self, levels=None):
@@ -99,16 +126,30 @@ class Tree (object):
             self.root.print()
 
     def post_order (self, aNode):
-        ...
+        if(aNode != None):
+            self.post_order(aNode.left)
+            self.post_order(aNode.right)
+            print(aNode.data, end=" ")
 
 # read in string expression from file
 def get_expr():
     my_file = open("./expression.txt", "r")
-    return my_file.readline().replace(" ", "")
+    return my_file.readline().split(" ")
 
 def main():
+    expr = get_expr()
     myTree = Tree()
-    myTree.create_tree(get_expr())
+    myTree.create_tree(expr)
     myTree.print()
+    print(f"{' '.join(expr)} = {myTree.evaluate(myTree.root)}\n")
+
+    print("Prefix Expression:", end=" ")
+    myTree.pre_order(myTree.root)
+    print("\n")
+
+    print("Postfix Expression:", end=" ")
+    myTree.post_order(myTree.root)
+    print()
+    print(f"todo: add floating number support")
 
 main()
