4ae5de1f1bf32d54210acc7a50a037a701a9efbe
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-11-11 21:58:04 -0600

    Begin assigment 19 (expression trees)

---
+++ assignments/19-expression-tree/ExpressionTree.py
@@ -0,0 +1,43 @@
+#  File: ExpressionTree.py
+
+#  Description: Assignment 19 | Expression Tree
+
+#  Student's Name: Matthew Maxwell
+
+#  Student's UT EID: mrm5632
+
+#  Course Name: CS 313E 
+
+#  Unique Number: 50205
+
+#  Date Created: 11-11-2019
+
+#  Date Last Modified: 11-11-2019
+
+class Stack (object):
+    ...
+
+class Node (object):
+    ...
+
+class Tree (object):
+    ...
+    def __init__ (self):
+        ...
+
+    def create_tree (self, expr):
+        ...
+
+    def evaluate (self, aNode):
+        ...
+
+    def pre_order (self, aNode):
+        ...
+
+    def post_order (self, aNode):
+        ...
+
+def main():
+    ...
+
+main()
