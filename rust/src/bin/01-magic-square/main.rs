511a5007c72487cb30ea03ed0bc05c3a18ebfc61
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-01-05 16:32:15 -0600

    chore: start problem 1

--- rust/src/bin/01-magic-square/main.rs
+++ rust/src/bin/01-magic-square/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+struct MagicSquare(Vec<Vec<usize>>);
+
+impl MagicSquare {
+    fn new(n: usize) -> Self {
+        assert_eq!(n % 2, 1);
+
+        Self(vec![vec![0; n]; n])
+    }
+
+    fn print(&self) {
+        for row in &self.0 {
+            for value in row {
+                print!("{} ", value);
+            }
+            println!();
+        }
+    }
+}
+
 pub fn main() {
-    println!("MAGIC SQUARE");
+    let magic_square = MagicSquare::new(3);
+    magic_square.print();
 }
